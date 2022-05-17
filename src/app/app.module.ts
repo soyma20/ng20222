f7b6b478d23966a821cbb29e5ab4776ebb4985a4
soyma20 <slaviksoyma2014@gmail.com>
2022-05-17 12:15:04 +0300

    my hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    PostsComponent,
-    PostComponent,
-    CommentsComponent,
-    CommentComponent
+
   ],
   imports: [
     BrowserModule,
