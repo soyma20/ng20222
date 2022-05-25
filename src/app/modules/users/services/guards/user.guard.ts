ecdabaf80eebb3c37bdbdcae8ce0a80d6c797acb
soyma20 <slaviksoyma2014@gmail.com>
2022-05-25 08:26:47 +0300

    my hw

---
+++ src/app/modules/users/services/guards/user.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanDeactivate<boolean>, CanActivate {
+  canDeactivate(component: boolean, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return confirm('close users?');
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return confirm('open users?');
+  }
+
+
+}
